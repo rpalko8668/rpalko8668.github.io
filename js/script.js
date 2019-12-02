8ab3c583358764dbe0f160c1d585136f54129710
rpalko8668 <51497884+rpalko8668@users.noreply.github.com>
2019-12-01 21:48:34 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ var myIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  let photos = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < photos.length; i++) {
+    photos[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
+  if (myIndex > photos.length) myIndex = 1;
+  photos[myIndex-1].style.display = "block";
   setTimeout(carousel, 3000);
 }
